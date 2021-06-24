f8bd651bdaec19d90f1e5c855a0637364d027b6c
[Błażej Karnecki] <blazej.karnecki@gmail.com>
2021-06-24 21:28:53 +0200

    [refactor] test structure

--- src/main/java/pl/karnecki/pokemonapi/Start.java
+++ src/main/java/pl/karnecki/pokemonapi/Start.java
@@ -11,16 +11,5 @@ public class Start {
 
     public Start(PokemonDao pokemonDao) {
         this.pokemonDao = pokemonDao;
-//
-//        carDao.saveCar(1L, "Toyota", "RAV4", "RED", 2015);
-//        carDao.saveCar(2L, "BMW", "760Li", "BLACK", 2021);
-//        carDao.saveCar(3L, "Audi", "A8", "BLUE", 2020);
-//        carDao.saveCar(4L, "Tesla", "S", "WHITE",2021);
-//        carDao.saveCar(5L, "Porsche", "Panamera", "GOLD", 2019);
-//        carDao.saveCar(6L, "Kia", "Proceed", "BLACK", 2018);
-//
-//        carDao.findByProductionYearRange(2015, 2019).forEach(System.out::println);
-
-//        carDao.findAll().forEach(System.out::println);
     }
 }
--- src/test/java/pl/karnecki/pokemonapi/controller/PokemonControllerTest.java
+++ src/test/java/pl/karnecki/pokemonapi/controller/PokemonControllerTest.java
@@ -4,15 +4,12 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.springframework.boot.test.context.SpringBootTest;
-import pl.karnecki.pokemonapi.dal.PokemonDao;
+import pl.karnecki.pokemonapi.dal.PokemonDaoImpl;
 import pl.karnecki.pokemonapi.model.Pokemon;
 import pl.karnecki.pokemonapi.service.PokemonService;
 import pl.karnecki.pokemonapi.service.PokemonServiceImpl;
-
-import java.util.Arrays;
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -24,7 +21,7 @@ class PokemonControllerTest {
 
 
     @Mock
-    private PokemonDao pokemonDao = mock(PokemonDao.class);
+    private PokemonDaoImpl pokemonDao = mock(PokemonDaoImpl.class);
 
 
     private PokemonService pokemonService = new PokemonServiceImpl(pokemonDao);
@@ -33,9 +30,7 @@ class PokemonControllerTest {
     public void setUpDao() {
         pokemonDao.savePokemon(1L, "Pikachu", "Electric");
         pokemonDao.savePokemon(2L, "Raichu", "Electric");
-        Pokemon pokemon1 = pokemonDao.getPokemonById(1L);
-        Pokemon pokemon2 = pokemonDao.getPokemonById(2L);
-        List<Pokemon> pokemons = Arrays.asList(pokemon1, pokemon2);
+        List<Pokemon> pokemons = pokemonDao.findAll();
         doReturn(pokemons).when(pokemonDao).findByName("Pikachu");
     }
 
@@ -44,7 +39,7 @@ class PokemonControllerTest {
     public void shouldReturnSelectedName() {
 
         //when
-        List<Pokemon> actual = pokemonService.getPokemonsByName("Pikachu");
+        List<Pokemon> actual = pokemonDao.findByName("Pikachu");
 
         //then
         Assertions.assertEquals("Pikachu", actual.get(0).getName());
